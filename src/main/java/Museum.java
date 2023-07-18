b6ceccb530dcbf3365af1314850de23c28bd040d
GeorgiaCD <georgiacrawforddellar@gmail.com>
2023-07-18 12:53:19 +0100

    added museum class body

--- src/main/java/Museum.java
+++ src/main/java/Museum.java
@@ -1,2 +1,22 @@
+import java.util.ArrayList;
+
 public class Museum {
+
+    private String name;
+    private ArrayList<Vehicle> vehicles;
+
+    public Museum(String name){
+        this.name = name;
+        this.vehicles = new ArrayList<>();
+    }
+
+    public int countVehicles(){
+        return vehicles.size();
+    }
+
+    public void addVehicles(Vehicle newVehicle){
+        this.vehicles.add(newVehicle);
+    }
+
 }
+
